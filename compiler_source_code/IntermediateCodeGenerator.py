cd42be9de5d959da48516d97b16e9b5ce30f2c69
Aq202 <diegoguatedb2002@gmail.com>
2024-10-02 14:11:46 -0600

    feat: equality operators CI

--- compiler_source_code/IntermediateCodeGenerator.py
+++ compiler_source_code/IntermediateCodeGenerator.py
@@ -5,7 +5,7 @@ from primitiveTypes import NumberType, StringType, NilType, BoolType
 from IntermediateCodeInstruction import SingleInstruction, EmptyInstruction, ConditionalInstruction
 from consts import MEM_ADDR_SIZE
 from Value import Value
-from IntermediateCodeTokens import FUNCTION, GET_ARG, RETURN, PARAM, RETURN_VAL, CALL, MULTIPLY, MALLOC, EQUAL, GOTO, LABEL
+from IntermediateCodeTokens import FUNCTION, GET_ARG, RETURN, PARAM, RETURN_VAL, CALL, MULTIPLY, MALLOC, EQUAL, NOT_EQUAL, GOTO, LABEL
 from antlr4 import tree
 from Offset import Offset
 
@@ -395,6 +395,46 @@ class IntermediateCodeGenerator():
       childNode = ctx.getChild(0)
       ctx.addr = childNode.addr
       return
+    
+    # Operación de comparación
+    code = None
+    numOperations = (len(ctx.children) - 1) // 2
+    
+    falseLabel = self.newLabel()
+    endLabel = self.newLabel()
+    
+    for i in range(numOperations):
+      
+      firstOperandIndex = i * 2
+      operand1 = ctx.getChild(firstOperandIndex).addr
+      operator = ctx.getChild(firstOperandIndex + 1).getText()
+      operand2 = ctx.getChild(firstOperandIndex + 2).addr
+      
+      # Se escoge el operador opuesto, para no seguir evaluando 
+      # Con uno que no sea igual (o diferente), la expresión completa es falsa
+      operator = EQUAL if operator != "==" else NOT_EQUAL
+      instruction = ConditionalInstruction(arg1=operand1, operator=operator, arg2=operand2, goToLabel=falseLabel)
+      
+      if code == None:
+        code = instruction
+      else:
+        code.concat(instruction)
+    
+    temp = self.newTemp()
+    # Si ninguna comparación es falsa, asignar trueVal a temporal y saltar al final
+    code.concat(SingleInstruction(result=temp, arg1=trueValue))
+    code.concat(SingleInstruction(operator=GOTO, arg1=endLabel))
+    
+    # Etiqueta de falso: asignar falseVal a temporal
+    code.concat(SingleInstruction(operator=LABEL, arg1=falseLabel))
+    code.concat(SingleInstruction(result=temp, arg1=falseValue))
+    
+    # Etiqueta de fin
+    code.concat(SingleInstruction(operator=LABEL, arg1=endLabel))
+    
+    # Guardar addr de resultado de operación de comparación y concatenar código
+    ctx.addr = temp
+    ctx.code.concat(code)
 
   def enterComparison(self, ctx: CompiscriptParser.ComparisonContext):
     if not self.continueCodeGeneration(): return
